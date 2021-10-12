f57855ddb436c5e2b65801e85118be065cb5f9de
Stypek123 <89249618+Stypek123@users.noreply.github.com>
2021-10-12 18:15:59 +0200

    jaka parowa wariacie

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 const app = express();
 
+// Middleware
 app.use(bodyParser.json())
 app.use(cors());
 
@@ -12,6 +13,15 @@ import users from './routes/api/users.js'
 
 app.use('/api/announcements', announcements)
 app.use('/api/users', users)
+
+// Handle production 
+if(process.env.NODE_ENV === 'production') {
+    // static folder
+    app.use(express.static(__dirname + 'public/'));
+
+    //Handle SPA 
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/indes.html'))
+}
 
 const port = process.env.PORT || 5000;
 
